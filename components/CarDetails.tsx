069d40000ffab246ae531388d61591525d6cf5ba
stephen-kovolski <s.kovolski@gmail.com>
2023-07-31 17:15:31 -0400

    Modal & Images

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,5 +1,7 @@
 import { CarProps } from '@/types';
+import {Fragment} from 'react'
 import React from 'react'
+import {Dialog, Transition} from '@headlessui/react'
 
 interface CarDetailsProps {
     isOpen: boolean;
